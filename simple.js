c9ee70282b8dcfabcb54125efb06925beccb6be7
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-02 13:40:58 +0100

    feat: mergeRight operator

--- simple.js
+++ simple.js
@@ -6,3 +6,4 @@ const equals = x => y => y === x;
 const where = obj => item => Object.keys(obj).every(key => obj[key](item[key]));
 const prop = key => obj => obj[key];
 const propEq = key => value => obj => obj[key] === value;
+const mergeRight = (obj1, obj2) => ({ ...obj1, ...obj2 });
