ae08061af655feeaa94058515f9d17e4f220d454
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-02 13:40:37 +0100

    feat: propEq operator

--- simple.js
+++ simple.js
@@ -5,3 +5,4 @@ const gte = x => y => y >= x;
 const equals = x => y => y === x;
 const where = obj => item => Object.keys(obj).every(key => obj[key](item[key]));
 const prop = key => obj => obj[key];
+const propEq = key => value => obj => obj[key] === value;
