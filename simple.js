9fa44fac82331af3cf05af12a4c614622266a6d6
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-02 13:40:02 +0100

    feat: where operator

--- simple.js
+++ simple.js
@@ -3,3 +3,4 @@ const lte = x => y => y <= x;
 const gt = x => y => y > x;
 const gte = x => y => y >= x;
 const equals = x => y => y === x;
+const where = obj => item => Object.keys(obj).every(key => obj[key](item[key]));
