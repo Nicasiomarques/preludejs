24383410231a6b5bcae0d018294bb356718bce2d
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-02 13:41:19 +0100

    feat: mergeRight pathOr

--- simple.js
+++ simple.js
@@ -7,3 +7,4 @@ const where = obj => item => Object.keys(obj).every(key => obj[key](item[key]));
 const prop = key => obj => obj[key];
 const propEq = key => value => obj => obj[key] === value;
 const mergeRight = (obj1, obj2) => ({ ...obj1, ...obj2 });
+const pathOr = (defaultValue, path) => obj => reduce((acc, key) => acc && acc[key], obj)(path) || defaultValue;
