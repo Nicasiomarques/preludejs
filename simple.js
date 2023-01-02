605b3454ca67a33f7da7df02c8394c304dd1a050
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-02 13:40:21 +0100

    feat: prop operator

--- simple.js
+++ simple.js
@@ -4,3 +4,4 @@ const gt = x => y => y > x;
 const gte = x => y => y >= x;
 const equals = x => y => y === x;
 const where = obj => item => Object.keys(obj).every(key => obj[key](item[key]));
+const prop = key => obj => obj[key];
