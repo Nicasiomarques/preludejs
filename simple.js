02b54f906703cbb4359d5f0dffeffaf40d1ea28e
Nicasiomarques <nicasiomarques18@gmail.com>
2023-01-02 13:49:58 +0100

    feat: either operator

--- simple.js
+++ simple.js
@@ -25,3 +25,4 @@ const last = xs => xs[len(xs) - 1];
 const sort = f => xs => xs.sort(f);
 const ifElse = (pred, f1, f2) => (...args) => pred(...args) ? f1(...args) : f2(...args);
 const both = (f, g) => x => f(x) && g(x);
+const either = (f, g) => x => f(x) || g(x);
